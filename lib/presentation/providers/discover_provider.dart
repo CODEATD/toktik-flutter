2271268f2e381a6810b503bd748d554e544fd47e
CODE ATD <codeatd@gmail.com>
2025-10-25 13:10:03 -0400

    81. Leer y asignar los videos al Provider

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/share/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  //TODO: Repository, DataSource
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJsonMap(video).toVideoPostEntity())
+        .toList();
 
-    //TODO: load videos
-
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
-
   }
 }
