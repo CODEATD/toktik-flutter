8f157ef8826ceaabf36e776237f4524ea4851a82
CODE ATD <codeatd@gmail.com>
2026-01-13 23:17:07 -0400

    90. Pausar videos

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -45,21 +45,30 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
             );
           }
 
-          return AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: Stack(
-              children: [
-                VideoPlayer(controller), // Gradient
-
-                // Text
-                Positioned(
-                  bottom: 50,
-                  left: 20,
-                  child: _VideoCaption(
-                    caption: widget.caption,
+          return GestureDetector(
+            onTap: () {
+              if( controller.value.isPlaying ) {
+                controller.pause();
+                return;
+              }
+              controller.play();
+            },
+            child: AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(
+                children: [
+                  VideoPlayer(controller), // Gradient
+            
+                  // Text
+                  Positioned(
+                    bottom: 50,
+                    left: 20,
+                    child: _VideoCaption(
+                      caption: widget.caption,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         });
