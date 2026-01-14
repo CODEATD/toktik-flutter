d5e013319d7d7d33dc9b2261967b869d636f56fd
CODE ATD <codeatd@gmail.com>
2026-01-13 23:02:15 -0400

    89. Reproducir videos

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -2,37 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class FullscreenPlayer extends StatefulWidget {
-
   final String videoUrl;
   final String caption;
 
-  const FullscreenPlayer({
-    super.key,
-    required this.videoUrl,
-    required this.caption
-  });
+  const FullscreenPlayer(
+      {super.key, required this.videoUrl, required this.caption});
 
   @override
   State<FullscreenPlayer> createState() => _FullscreenPlayerState();
 }
 
 class _FullscreenPlayerState extends State<FullscreenPlayer> {
-
   late VideoPlayerController controller;
 
   @override
   void initState() {
     super.initState();
 
-    controller = VideoPlayerController.asset( widget.videoUrl )
-    ..setVolume(0)
-    ..setLooping(true)
-    ..play();
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
   }
 
   @override
   void dispose() {
-
     controller.dispose();
 
     super.dispose();
@@ -40,15 +34,55 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
 
   @override
   Widget build(BuildContext context) {
-
     return FutureBuilder(
-      future: controller.initialize(), 
-      builder: (context, snapshot) {
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 2,
+              ),
+            );
+          }
 
-        return const Center(
-          child: CircularProgressIndicator( strokeWidth: 2, ),
-        );
-      }
+          return AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller), // Gradient
+
+                // Text
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(
+                    caption: widget.caption,
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+  final String caption;
+
+  const _VideoCaption({super.key, required this.caption});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ),
     );
   }
 }
